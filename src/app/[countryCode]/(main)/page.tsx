c652ec081e6d9d999b231baa5eecb9c0ee05ede6
Manish-Builder-io <manish@builder.io>
2025-02-11 17:38:49 +0530

    First psh with builder integration

--- src/app/[countryCode]/(main)/page.tsx
+++ src/app/[countryCode]/(main)/page.tsx
@@ -1,36 +1,48 @@
 import { Metadata } from "next"
+import { builder } from "@builder.io/sdk"
+import { RenderBuilderContent } from "@modules/common/components/builder"
 
 import FeaturedProducts from "@modules/home/components/featured-products"
-import Hero from "@modules/home/components/hero"
 import { listCollections } from "@lib/data/collections"
 import { getRegion } from "@lib/data/regions"
 
+// Initialize Builder.io with your Public API Key
+builder.init("e49353dd3063449e9d4dc86b6a2c9d36")
+
 export const metadata: Metadata = {
-  title: "Medusa Next.js Starter Template",
+  title: "Builder.io + Medusa Next.js Starter Template",
   description:
     "A performant frontend ecommerce starter template with Next.js 14 and Medusa.",
 }
 
-export default async function Home(props: {
-  params: Promise<{ countryCode: string }>
-}) {
-  const params = await props.params
+interface HomeProps {
+  params: {
+    countryCode: string
+  }
+}
 
+export default async function Home({ params }: HomeProps) {
   const { countryCode } = params
 
   const region = await getRegion(countryCode)
-
-  const { collections } = await listCollections({
-    fields: "id, handle, title",
-  })
+  const { collections } = await listCollections({ fields: "id, handle, title" })
 
   if (!collections || !region) {
     return null
   }
 
+  const content = await builder
+    .get("page", {
+      userAttributes: { urlPath: "/" },
+      options: {countryCode},
+      locale: countryCode,
+      prerender: false,
+    })
+    .toPromise()
+
   return (
     <>
-      <Hero />
+      <RenderBuilderContent content={content} model="page"  locale={countryCode} />
       <div className="py-12">
         <ul className="flex flex-col gap-x-6">
           <FeaturedProducts collections={collections} region={region} />
